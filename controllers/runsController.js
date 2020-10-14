25253dcc782cf7d4aeec4bbe3261a23a6843ffcc
benjaminclosson1 <benjaminclosson1@gmail.com>
2020-10-13 19:05:51 -0700

    Commit added run post create route

--- controllers/runsController.js
+++ controllers/runsController.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 //Database
 const db = require('../models');
+const { populate } = require('../models/Run');
 
 // Current path = '/runs'
 
@@ -31,6 +32,31 @@ router.get('/new', (req, res) => {
     });
 });
 
+// GET Show
+router.get('/:runId', (req, res) => {
+    db.Run.findById(req.params.runId)
+    .populate('workout')
+    .exec((err, runById) => {
+        if (err) return console.log(err);
+
+        console.log('runs/show', runById);
+    });
+});
+
 // POST Create
+router.post('/', (req, res) => {
+    db.Run.create(req.body, (err, newRun) => {
+        if (err) return console.log(err);
+        db.Workout.findById(req.body.workout, (err, foundWorkout) => {
+            if (err) return console.log(err);
+            foundWorkout.runs.push(newRun._id);
+            foundWorkout.save((err, savedWorkout) => {
+                if (err) return console.log(err);
+                res.redirect(`workouts, ${newRun.id}`);
+            })
+        })
+    });
+});
+
 // GET Edit
 module.exports = router;
