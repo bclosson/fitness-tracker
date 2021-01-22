34a647c33200acbf5b81b6c87aa06fa2b7335b2d
benjaminclosson1 <benjaminclosson1@gmail.com>
2021-01-21 22:23:31 -0800

    commit created user register

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,96 +1,165 @@
-const express = require('express');
+const express = require("express");
+const { check, validationResult } = require("express-validator/check");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
 
 // Database
-const db = require('../models');
+const db = require("../models");
+const { User } = require("../models");
 
 // Current Path = '/users'
 
 // Get Index Route
-router.get('/', (req, res) => {
-    // Query DB for all users
-    db.User.find({}, (err, allUsers) => {
-        if (err) return console.log(err);
+router.get("/", (req, res) => {
+  // Query DB for all users
+  db.User.find({}, (err, allUsers) => {
+    if (err) return console.log(err);
 
-        const context = {
-            users: allUsers,
-            name: 'All Users'
-        };
+    const context = {
+      users: allUsers,
+      name: "All Users",
+    };
 
-        res.render('users/index', context);
-    });
+    res.render("users/index", context);
+  });
 });
 
 // Get New Route
-router.get('/new', (req, res) => {
-    res.render('users/new');
+router.get("/new", (req, res) => {
+  res.render("users/new");
 });
 
 // Get Show Route
-router.get('/:userId', (req, res) => {
-    // Query DB for user by Id
-    db.User.findById(req.params.userId)
-    .populate('workouts')
+router.get("/:userId", (req, res) => {
+  // Query DB for user by Id
+  db.User.findById(req.params.userId)
+    .populate("workouts")
     .exec((err, foundUser) => {
-        if (err) return console.log(err);
-        const context = {
-            user: foundUser,
-        };
-        res.render('users/show', context);
+      if (err) return console.log(err);
+      const context = {
+        user: foundUser,
+      };
+      res.render("users/show", context);
     });
 });
 
-// Post Create
-router.post('/', (req, res) => {
-    // Query DB to create new user
-    db.User.create(req.body, (err, newUser) => {
-        if (err) return console.log(err);
-        
-        res.redirect('/users');
-    });
-});
+// // Post Create
+// router.post('/', (req, res) => {
+//     // Query DB to create new user
+//     db.User.create(req.body, (err, newUser) => {
+//         if (err) return console.log(err);
+
+//         res.redirect('/users');
+//     });
+// });
+
+// New Post Create
+router.post(
+  "/",
+  [
+    check("username", "Please enter a valid username").not().isEmpty(),
+    check("email", "Please enter a valid email").isEmail(),
+    check("password", "Please enter a valid password").isLength({
+      min: 6,
+    }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+      });
+    }
+    const { username, email, password } = req.body;
+    try {
+      let user = await User.findOne({
+        email,
+      });
+      if (user) {
+        return res.status(400).json({
+          msg: "User Already Exists",
+        });
+      }
+
+      user = new User({
+        username,
+        email,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        "randomString",
+        {
+          expiresIn: 10000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.status(200).json({
+            token,
+          });
+        }
+      );
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Error in Saving");
+    }
+  }
+);
 
 // Get Edit Route
-router.get('/:userId/edit', (req, res) => {
-    // Query DB for user by ID
-    db.User.findById(req.params.userId, (err, foundUser) => {
-        if (err) return console.log(err);
-        
-        const context = {
-            user: foundUser,
-        };
-        res.render('users/edit', context);
-    });
+router.get("/:userId/edit", (req, res) => {
+  // Query DB for user by ID
+  db.User.findById(req.params.userId, (err, foundUser) => {
+    if (err) return console.log(err);
+
+    const context = {
+      user: foundUser,
+    };
+    res.render("users/edit", context);
+  });
 });
 
 // Put Update Route
-router.put('/:userId', (req, res) => {
-    // Validate DATA!
-    //Query DB to update user by ID
-    db.User.findByIdAndUpdate(
-        req.params.userId,
-        req.body,
-        {new: true},
-        (err, updatedUser) => {
-            if (err) return console.log(err);
-            
-            res.redirect(`/users/${updatedUser._id}`);
-        }
-    );
+router.put("/:userId", (req, res) => {
+  // Validate DATA!
+  //Query DB to update user by ID
+  db.User.findByIdAndUpdate(
+    req.params.userId,
+    req.body,
+    { new: true },
+    (err, updatedUser) => {
+      if (err) return console.log(err);
+
+      res.redirect(`/users/${updatedUser._id}`);
+    }
+  );
 });
 
 // Delete Route
-router.delete('/:userId', (req, res) => {
-    // Qeury DB to delete user by ID
-    db.User.findByIdAndDelete(req.params.userId, (err, deletedUser) => {
-        if (err) return console.log(err);
-        
-        db.Workout.deleteMany({_id: { $in: deletedUser.workouts }}, (err) => {
-            if (err) return console.log(err);
+router.delete("/:userId", (req, res) => {
+  // Qeury DB to delete user by ID
+  db.User.findByIdAndDelete(req.params.userId, (err, deletedUser) => {
+    if (err) return console.log(err);
 
-            res.redirect('/users');
-        })
+    db.Workout.deleteMany({ _id: { $in: deletedUser.workouts } }, (err) => {
+      if (err) return console.log(err);
+
+      res.redirect("/users");
     });
+  });
 });
 
 module.exports = router;
