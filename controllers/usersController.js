4f723cffc9ffd4f3c88b336dac4a82efac8bd2d8
benjaminclosson1 <benjaminclosson1@gmail.com>
2021-01-22 15:33:42 -0800

    commit changed user register

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const { check, validationResult } = require("express-validator/check");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const router = express.Router();
@@ -55,69 +54,28 @@ router.get("/:userId", (req, res) => {
 // });
 
 // New Post Create
-router.post(
-  "/",
-  [
-    check("username", "Please enter a valid username").not().isEmpty(),
-    check("email", "Please enter a valid email").isEmail(),
-    check("password", "Please enter a valid password").isLength({
-      min: 6,
-    }),
-  ],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({
-        errors: errors.array(),
-      });
-    }
-    const { username, email, password } = req.body;
-    try {
-      let user = await db.User.findOne({
-        email,
-      });
-      if (user) {
-        return res.status(400).json({
-          msg: "User Already Exists",
-        });
-      }
-
-      user = new db.User({
-        username,
-        email,
-        password,
-      });
-
-      const salt = await bcrypt.genSalt(10);
-      user.password = await bcrypt.hash(password, salt);
-
-      await user.save();
-
-      const payload = {
-        user: {
-          id: user.id,
-        },
-      };
-
-      jwt.sign(
-        payload,
-        "randomString",
-        {
-          expiresIn: 10000,
-        },
-        (err, token) => {
-          if (err) throw err;
-          res.status(200).json({
-            token,
-          });
-        }
-      );
-    } catch (err) {
-      console.log(err.message);
-      res.status(500).send("Error in Saving");
-    }
+router.post("/", async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    const user = {
+      name: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    };
+    console.log(salt);
+    console.log(hashedPassword);
+    db.User.create(user);
+    res.status(201).send();
+  } catch {
+    res.status(500).send();
   }
-);
+});
+
+// User Login
+router.post("/login", async (req, res) => {
+  const user = db.User.find((user) => user_id);
+});
 
 // Get Edit Route
 router.get("/:userId/edit", (req, res) => {
