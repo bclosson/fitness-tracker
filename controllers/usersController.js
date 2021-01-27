34ac7034727f946272126714531d8bce9d31902a
benjaminclosson1 <benjaminclosson1@gmail.com>
2021-01-27 13:30:59 -0800

    commit added placeholder to login/register ejs forms

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -25,82 +25,82 @@ router.get("/", (req, res) => {
   });
 });
 
-// Get New Route
-router.get("/new", (req, res) => {
-  res.render("users/new");
-});
+// // Get New Route
+// router.get("/new", (req, res) => {
+//   res.render("users/new");
+// });
 
-// Login Route
-router.get("/login", (req, res) => {
-  res.render("users/login");
-});
+// // Login Route
+// router.get("/login", (req, res) => {
+//   res.render("users/login");
+// });
 
 // NEW/CREATE VALIDATION -------------------------------
-router.post("/", async (req, res) => {
-  // validate the user
-  const { error } = registerValidation(req.body);
-  if (error) return res.status(400).json({ error: error.details[0].message });
-  const isEmailExist = await User.findOne({ email: req.body.email });
+// router.post("/", async (req, res) => {
+//   // validate the user
+//   const { error } = registerValidation(req.body);
+//   if (error) return res.status(400).json({ error: error.details[0].message });
+//   const isEmailExist = await User.findOne({ email: req.body.email });
 
-  if (isEmailExist)
-    return res.status(400).json({ error: "Email already exists" });
-  // hash the password
-  const salt = await bcrypt.genSalt(10);
-  const password = await bcrypt.hash(req.body.password, salt);
+//   if (isEmailExist)
+//     return res.status(400).json({ error: "Email already exists" });
+//   // hash the password
+//   const salt = await bcrypt.genSalt(10);
+//   const password = await bcrypt.hash(req.body.password, salt);
 
-  const user = new User({
-    username: req.body.username,
-    email: req.body.email,
-    password, //hashed password
-  });
+//   const user = new User({
+//     username: req.body.username,
+//     email: req.body.email,
+//     password, //hashed password
+//   });
 
-  try {
-    const savedUser = await user.save();
-    res.json({ error: null, data: savedUser._id });
-    // await user.save();
-    // res.render("users/login");
-  } catch (error) {
-    res.status(400).json({ error });
-  }
-});
+//   try {
+//     const savedUser = await user.save();
+//     res.json({ error: null, data: savedUser._id });
+//     // await user.save();
+//     // res.render("users/login");
+//   } catch (error) {
+//     res.status(400).json({ error });
+//   }
+// });
 
-// USER LOGIN ---------------------------------------------
-router.post("/login", async (req, res) => {
-  // validate the user
-  const { error } = loginValidation(req.body);
+// // USER LOGIN ---------------------------------------------
+// router.post("/login", async (req, res) => {
+//   // validate the user
+//   const { error } = loginValidation(req.body);
 
-  // throw validation errors
-  if (error) return res.status(400).json({ error: error.details[0].message });
+//   // throw validation errors
+//   if (error) return res.status(400).json({ error: error.details[0].message });
 
-  const user = await User.findOne({ email: req.body.email });
+//   const user = await User.findOne({ email: req.body.email });
 
-  // throw error when email is wrong
-  if (!user) return res.status(400).json({ error: "Email is incorrect" });
+//   // throw error when email is wrong
+//   if (!user) return res.status(400).json({ error: "Email is incorrect" });
 
-  // check for password match
-  const validPassword = await bcrypt.compare(req.body.password, user.password);
-  if (!validPassword)
-    return res.status(400).json({
-      error: "Password is incorrect",
-    });
+//   // check for password match
+//   const validPassword = await bcrypt.compare(req.body.password, user.password);
+//   if (!validPassword)
+//     return res.status(400).json({
+//       error: "Password is incorrect",
+//     });
 
-  // create token
-  const token = jwt.sign(
-    // payload data
-    {
-      name: user.username,
-      id: user._id,
-    },
-    process.env.ACCESS_TOKEN_SECRET
-  );
+//   // create token
+//   const token = jwt.sign(
+//     // payload data
+//     {
+//       name: user.username,
+//       id: user._id,
+//     },
+//     process.env.ACCESS_TOKEN_SECRET
+//   );
 
-  res.header("auth-token", token).json({
-    error: null,
-    data: {
-      token,
-    },
-  });
-});
+//   res.header("auth-token", token).json({
+//     error: null,
+//     data: {
+//       token,
+//     },
+//   });
+// });
 
 // Get Show Route
 router.get("/:userId", (req, res) => {
