4c356b7ddf2f1c1a2a7129d877c8149cf40a5a8a
benjaminclosson1 <benjaminclosson1@gmail.com>
2021-02-01 17:27:08 -0800

    commit deleted dashboard files, moved auth views to user views folder

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,8 @@
 module.exports = {
-  auth: require("./authController"),
   users: require("./usersController"),
   workouts: require("./workoutsController"),
   runs: require("./runsController"),
   bikes: require("./bikesController"),
   hiits: require("./hiitsController"),
   lifts: require("./liftsController"),
-  dashboard: require("./dashboardController"),
 };
